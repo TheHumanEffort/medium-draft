7061c35de2fd350e59ba34953acbe545668d7a7f
Daniel Staudigel <dstaudigel@gmail.com>
2017-02-11 15:46:30 -0800

    Allow for the setting of the subject.

--- src/components/blocks/image.js
+++ src/components/blocks/image.js
@@ -1,36 +1,102 @@
 import React, { PropTypes } from 'react';
 
+import Immutable from 'immutable';
 import { Entity, EditorBlock } from 'draft-js';
 
 import { getCurrentBlock, updateDataOfBlock } from '../../model/';
 
 var ImageBlock = React.createClass({
-  updateStyle: function (style) {
+  updateStyle: function(style) {
+    this.updateData({style: style});
+  },
+
+  getInitialState: function() {
+    return { settingSubject: false };
+  },
+
+  updateData: function(hash) {
     const { block, blockProps } = this.props;
     const { setEditorState, getEditorState } = blockProps;
 
     const data = this.props.block.getData();
-    const newData = data.set('style', style);
+
+    const newData = data.merge(Immutable.fromJS(hash));
 
     this.props.blockProps.setEditorState(updateDataOfBlock(
       this.props.blockProps.getEditorState(), block, newData)
-    );
+                                        );
+
   },
 
-  floatLeft: function () {
+  get: function(key) {
+    return this.props.block.getData().get(key);
+  },
+
+  floatLeft: function(e) {
     this.updateStyle('left');
+    e.stopPropagation();
+    e.preventDefault();
   },
 
-  floatRight: function () {
+  floatRight: function(e) {
     this.updateStyle('right');
+    e.stopPropagation();
+    e.preventDefault();
   },
 
-  fullWidth: function () {
+  fullWidth: function(e) {
     this.updateStyle('full');
+    e.stopPropagation();
+    e.preventDefault();
   },
 
-  default: function () {
+  default: function(e) {
     this.updateStyle(null);
+    e.stopPropagation();
+    e.preventDefault();
+  },
+
+  setSubject: function(e) {
+    this.setState({ settingSubject: true });
+
+    e.stopPropagation();
+    e.preventDefault();
+  },
+
+  subjectClick: function(e) {
+    if (this.state.settingSubject) {
+      var x = e.nativeEvent.offsetX / e.currentTarget.offsetWidth;
+      var y = e.nativeEvent.offsetY / e.currentTarget.offsetHeight;
+
+      this.updateData({
+        subjectX: x, subjectY: y,
+      });
+
+      this.setState({ settingFocus: false });
+
+      e.stopPropagation();
+      e.preventDefault();
+    }
+  },
+
+  subjectBox: function() {
+    var x = this.get('subjectX');
+    var y = this.get('subjectY');
+    if (!x || !y) return [];
+
+    var style = {
+      left: (Math.floor(x * 100) - 5) + '%',
+      right: (Math.floor((1 - x) * 100) - 5) + '%',
+      top: (Math.floor(y * 100) - 5) + '%',
+      bottom: (Math.floor((1 - y) * 100) - 5) + '%',
+      backgroundColor: 'rgba(255,255,255,0.4)',
+      border: '1px thin blue',
+      position: 'absolute',
+    };
+
+    return (
+        <div className='subject-highlight' style={ style }>Subject</div>
+    );
   },
 
   render() {
@@ -54,14 +120,16 @@ var ImageBlock = React.createClass({
     if (src !== null) {
       return (
         <div className={outerClass}>
-          <div className="md-block-image-inner-container">
-            <div className="md-block-image-inner-container__operations" contentEditable="false" suppressContentEditableWarning={true}>
-              <button onClick={this.floatLeft} disabled={ this.props.block.getData().get('style') == 'left'}>Left </button>
-              <button onClick={this.floatRight } disabled={ this.props.block.getData().get('style') == 'right' }>Right  </button>
-              <button onClick={this.fullWidth } disabled={ this.props.block.getData().get('style') == 'full' }>Wide</button>
-              <button onClick={this.default} disabled={!this.props.block.getData().get('style')}>Default</button>
-            </div>
-            <img role="presentation" className={className} src={src} />
+          <div className='md-block-image-inner-container' onClick={this.subjectClick}>
+            <div className='md-block-image-inner-container__operations' contentEditable='false' suppressContentEditableWarning={true}>
+              <button onClick={this.floatLeft} disabled={ style == 'left'}>Left </button>
+              <button onClick={this.floatRight } disabled={ style == 'right' }>Right  </button>
+              <button onClick={this.fullWidth } disabled={ style == 'full' }>Wide</button>
+              <button onClick={this.default} disabled={!style}>Default</button>
+              <button onClick={this.setSubject}>Set Focus</button>
+          </div>
+          { this.subjectBox() }
+            <img role='presentation' className={className} src={src} />
           </div>
           <figcaption>
             <EditorBlock {...props} />
